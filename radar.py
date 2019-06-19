aa100f737b73f4f13113810fa545249c654fde70
masashi <masashi@sybot>
2019-06-18 23:40:42 -0300

    Radar 5.1 - Removendo código desnecessário

--- radar.py
+++ radar.py
@@ -45,43 +45,6 @@ def main():
             tela(win)
 
 
-    #print("Tempo: {0}\nStatus: {1}\nVoo: {2}\nDistancia: {3}\nVelocidade: {4}\nX: {5}\nY: {6}\nZ: {7}\n".format(tempo,status,voo,dist,vel,x,y,z))
-    
-    ''' 
-    Mostrar informação acima do avião
-    x = -200
-    y = 200
-    angulo = 45
-    txt = Text(Point(x,y+20), "TESTE")
-    txt.setSize(5)
-    txt.setTextColor("red")
-    txt.draw(win)
-    aviao(x,y,"red",win)
-    '''
-
-
-    '''   
-    # PLOTAGEM DO AVIAO
-    a = 0
-    x = -200
-    y = 200
-    while a <=10:    
-        aviao(x,y,"red",win)
-        x+=70
-        y+=0
-        a+=1
-        sleep(2)
-        win.close()
-        if win.isClosed():
-            win = GraphWin("", 1000, 1000)
-            win.setCoords(-500, -500, 500, 500)
-            win.setBackground(color_rgb(0, 0, 0))
-            # Desenhando  linhas e circulos
-            tela(win)        
-    '''
-    # FIM DA PLOTAGEM 
-
-
     win.getMouse()
     win.close()
     # Fechando Janela/Radar
